610c25d585ebb090087b8c8fbe1fe494f368847e
John Price <john.price@digamesystems.com>
2022-07-01 07:34:49 -0700

    Initial commit

---
+++ network.h
@@ -0,0 +1,3 @@
+String SSID = "Bighead";
+String PASSWORD = "billgates";
+String IP_ADDRESS = "192.168.1.179";
---
+++ test_websocket_client.ino
@@ -0,0 +1,130 @@
+/*
+ * WebSocketClient.ino
+ *
+ *  Created on: 24.05.2015
+ *
+ */
+
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
+#include <WebSocketsClient.h>
+
+#include "network.h"
+
+
+WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+
+
+
+#define USE_SERIAL Serial
+bool showData = false; 
+
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+	const uint8_t* src = (const uint8_t*) mem;
+	USE_SERIAL.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+	for(uint32_t i = 0; i < len; i++) {
+		if(i % cols == 0) {
+			USE_SERIAL.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+		}
+		USE_SERIAL.printf("%02X ", *src);
+		src++;
+	}
+	USE_SERIAL.printf("\n");
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+
+	switch(type) {
+		case WStype_DISCONNECTED:
+			USE_SERIAL.printf("[WSc] Disconnected!\n");
+			break;
+		case WStype_CONNECTED:
+			USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
+
+			// send message to server when Connected
+			webSocket.sendTXT("Connected");
+			break;
+		case WStype_TEXT:
+      if (showData) USE_SERIAL.printf("%s", payload);
+
+			// send message to server
+			// webSocket.sendTXT("message here");
+			break;
+		case WStype_BIN:
+			USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
+			hexdump(payload, length);
+
+			// send data to server
+			// webSocket.sendBIN(payload, length);
+			break;
+		case WStype_ERROR:			
+		case WStype_FRAGMENT_TEXT_START:
+		case WStype_FRAGMENT_BIN_START:
+		case WStype_FRAGMENT:
+		case WStype_FRAGMENT_FIN:
+			break;
+	}
+
+}
+
+void setup() {
+	// USE_SERIAL.begin(921600);
+	USE_SERIAL.begin(115200);
+
+	//Serial.setDebugOutput(true);
+	USE_SERIAL.setDebugOutput(true);
+
+	USE_SERIAL.println();
+	USE_SERIAL.println();
+	USE_SERIAL.println();
+
+	for(uint8_t t = 4; t > 0; t--) {
+		USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\n", t);
+		USE_SERIAL.flush();
+		delay(1000);
+	}
+  USE_SERIAL.println(SSID);
+  USE_SERIAL.println(PASSWORD);
+	WiFiMulti.addAP(SSID.c_str(), PASSWORD.c_str());
+
+	//WiFi.disconnect();
+	while(WiFiMulti.run() != WL_CONNECTED) {
+		delay(100);
+	}
+
+  USE_SERIAL.println(IP_ADDRESS);
+	// server address, port and URL
+	webSocket.begin(IP_ADDRESS.c_str(), 81, "/");
+
+	// event handler
+	webSocket.onEvent(webSocketEvent);
+
+	// use HTTP Basic Authorization this is optional remove if not needed
+	//webSocket.setAuthorization("user", "Password");
+
+	// try ever 5000 again if connection has failed
+	webSocket.setReconnectInterval(5000);
+  USE_SERIAL.println("RUNNING!");
+}
+
+void loop() {
+  String inString="";
+  
+	webSocket.loop();
+
+  // A couple of key commands to turn on/off logging
+  if (USE_SERIAL.available()){
+    inString = USE_SERIAL.readStringUntil('\n');
+    inString.trim();
+    if (inString == "["){
+      showData=false;
+    }else if (inString == "]"){
+      showData=true;
+    };
+  }
+}
